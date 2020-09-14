09105846ed15f69a93a80fa02f87fb91b5026de4
James <jtsetsekas@gmail.com>
2020-09-14 10:40:05 -0400

    first commit
    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDBVJWBtYHpwaC6V2fVh1a3mXbiMQZrN5k",
+    authDomain: "catch-of-the-day-7955a.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-7955a.firebaseio.com",
+});
+
+export default base;
